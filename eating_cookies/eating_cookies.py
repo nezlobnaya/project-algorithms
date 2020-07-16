5b07c5d7966b0c995d430eeb734f99a80ae085d8
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-16 16:04:42 -0400

    refactored eating cookie algo using cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,17 +2,32 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+# def eating_cookies(n):
+#     # Your code here
+#     if n == 0:
+#         return 1
+#     elif n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     else:
+#         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+def eating_cookies(n, cache = None):
     # Your code here
+    if n < 0:
+        return 0
     if n == 0:
         return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
-
+    if cache is None:
+        cache = {}
+    elif isinstance(cache, list):
+        cache = dict.fromkeys(cache)
+    if n in cache:
+        return cache[n]
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    
+    return cache[n]
 
 
 if __name__ == "__main__":
