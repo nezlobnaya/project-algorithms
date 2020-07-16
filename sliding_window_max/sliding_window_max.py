151a39fbdeb0b39def3f06f82d53fc628dee3f98
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-16 18:29:38 -0400

    sliding window max solution using deque added,fastest

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,19 +2,32 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
-    arrlen = len(nums)
+# def sliding_window_max(nums, k):
+#     # Your code here
+#     arrlen = len(nums)
    
-    start = 0
-    stop = k
-    highest = []
-    while stop <= arrlen:
-        highest.append(max(nums[start:stop]))
-        start+=1
-        stop+=1
-    return highest
+#     start = 0
+#     stop = k
+#     highest = []
+#     while stop <= arrlen:
+#         highest.append(max(nums[start:stop]))
+#         start+=1
+#         stop+=1
+#     return highest
     
+import collections
+def sliding_window_max(nums, k):
+        dq = collections.deque()
+        ans = []
+        for i in range(len(nums)):
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
+            dq.append(i)
+            if dq[0] == i - k:
+                dq.popleft()
+            if i >= k - 1:
+                ans.append(nums[dq[0]])
+        return ans
 
 
 if __name__ == '__main__':
