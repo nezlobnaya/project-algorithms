2b348e3ca619f2329a94b0e149f5d018e87c2df2
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-16 19:10:00 -0400

    added comments to sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,26 +4,33 @@ Returns: a List of integers
 '''
 # def sliding_window_max(nums, k):
 #     # Your code here
-#     arrlen = len(nums)
+    # arrlen = len(nums)
    
-#     start = 0
-#     stop = k
-#     highest = []
-#     while stop <= arrlen:
-#         highest.append(max(nums[start:stop]))
-#         start+=1
-#         stop+=1
-#     return highest
+    # start = 0
+    # stop = k
+    # highest = []
+    # while stop <= arrlen:
+    #     highest.append(max(nums[start:stop]))
+    #     start+=1
+    #     stop+=1
+    # return highest
     
 import collections
 def sliding_window_max(nums, k):
         dq = collections.deque()
         ans = []
         for i in range(len(nums)):
+            # For every element, the previous  
+            # smaller elements are useless 
+            # so remove them from dq 
             while dq and nums[dq[-1]] <= nums[i]:
                 dq.pop()
+            # Add new element at rear of queue 
             dq.append(i)
+            # Remove the elements which are  
+            # out of this window 
             if dq[0] == i - k:
+                # remove from front of deque
                 dq.popleft()
             if i >= k - 1:
                 ans.append(nums[dq[0]])
