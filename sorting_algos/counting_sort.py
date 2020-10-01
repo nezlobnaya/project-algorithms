d9bf6e77d06f7e317c502b702f1441d2dee1beec
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-01 11:30:34 -0400

    description added to count_sort

--- sorting_algos/counting_sort.py
+++ sorting_algos/counting_sort.py
@@ -1,3 +1,10 @@
+"""
+Counting sort is a sorting algorithm that sorts the elements of an array 
+by counting the number of occurrences of each unique element in the array. 
+The count is stored in an auxiliary array and the sorting is done 
+by mapping the count as an index of the auxiliary array.
+
+"""
 #  Write a function that takes:
 
 #     a list of unsorted_scores
