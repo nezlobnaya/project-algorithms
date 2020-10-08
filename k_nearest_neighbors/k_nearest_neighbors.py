27ba28d74e4669deee4e9762b9927930459195d2
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-08 17:23:36 -0400

    k_nearest_neighbors algo added

---
+++ k_nearest_neighbors/k_nearest_neighbors.py
@@ -0,0 +1,34 @@
+import warnings
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
+new_features = [5, 7]
+
+[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(new_features[0], new_features[1])
+plt.show()
+
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less that total voting groups! Idiot!')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    print('Tuple:', Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
+
+    return vote_result
+
+
+result = k_nearest_neighbors(dataset, new_features, k=3)
+print('Result:' ,result)
