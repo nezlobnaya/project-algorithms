8dd8ec001116adc0eee2cddd7f245d9f78a168ef
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-17 15:35:12 -0500

    is first come first served true or false algo added to interview cake folder

--- interview_cake/merge_arrays.py
+++ interview_cake/merge_arrays.py
@@ -3,30 +3,22 @@ import unittest
 
 def merge_lists(my_list, alices_list):
 
-    # Combine the sorted lists into one large sorted list
     size = len(my_list) + len(alices_list)
     merged_list = [None] * size
-    
-    cur_ptr_alices = cur_ptr_mine = cur_ptr_merged = 0
-    
+
+    cur_ptr_mine = cur_ptr_alices = cur_ptr_merged = 0
+
     while cur_ptr_merged < size:
-        is_mylist_out = cur_ptr_mine >= len(my_list)
-        is_alices_list_out = cur_ptr_alices >= len(alices_list)
-        
-        if (not is_mylist_out and
-                (is_alices_list_out or my_list[cur_ptr_mine] < alices_list[cur_ptr_alices])):
-            
+        is_mine_out = cur_ptr_mine >= len(my_list)
+        is_alices_out = cur_ptr_alices >=len(alices_list)
+
+        if (not is_mine_out and (is_alices_out or my_list[cur_ptr_mine] < alices_list[cur_ptr_alices])):
             merged_list[cur_ptr_merged] = my_list[cur_ptr_mine]
-            cur_ptr_mine +=1
-                
+            cur_ptr_mine+=1
         else:
             merged_list[cur_ptr_merged] = alices_list[cur_ptr_alices]
-            cur_ptr_alices +=1
-        
+            cur_ptr_alices+=1
         cur_ptr_merged+=1
-            
-        
-
     return merged_list
 
 
