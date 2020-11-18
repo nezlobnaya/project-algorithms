cd40ad529b15a1012703427bf4d5dea7fef1104f
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-18 10:04:08 -0500

    has palindrome permutation solution added to interview cake

---
+++ interview_cake/has_palindrome_permutation.py
@@ -0,0 +1,62 @@
+import unittest
+
+
+def has_palindrome_permutation(the_string):
+
+    # Check if any permutation of the input is a palindrome
+    odd_chars = set()
+    for char in the_string:
+        if char in odd_chars:
+            odd_chars.remove(char)
+        else:
+            odd_chars.add(char)
+
+    return len(odd_chars) <=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
