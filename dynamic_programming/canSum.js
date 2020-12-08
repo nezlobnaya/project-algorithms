45b64cac38851f408cd66b5eac72ac84f0557495
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-07 22:01:43 -0500

    dynamic programming challenges with solutions added to a new directory

---
+++ dynamic_programming/canSum.js
@@ -0,0 +1,48 @@
+/*Write a function `canSum(targetSum, numbers)` that takes in a targetSum 
+and an array of numbers as arguments
+The function should return a boolean indication whether or 
+not it is possible to generate the targetSum using numbers from the array*/
+
+// const canSum = (targetSum, numbers) => {
+//     if (targetSum === 0) return true;
+//     if (targetSum < 0) return false;
+    
+//     for (let num of numbers) {
+//         const remainder = targetSum - num;
+//         if (canSum(remainder, numbers) === true) {
+//             return true;
+//         }
+
+//     }
+//     return false;
+// }
+
+const canSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+    
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum] = true;
+            return true;
+        }
+
+    }
+    memo[targetSum] = false;
+    return false;
+}
+
+
+
+console.log(canSum(7, [2, 3]));// true
+console.log(canSum(7, [5, 3, 4, 7]));// true
+console.log(canSum(7, [2, 4]));// false
+console.log(canSum(8, [2, 3, 5]));// true
+console.log(canSum(300, [7, 14]));// true
+
+//Time/Space
+// brute force recursion: O(n^m)time  O(m) space
+
+// memoized O(m*n)time O(m) space
---
+++ dynamic_programming/gridTraveler.js
@@ -0,0 +1,35 @@
+// you are a traveler on a 2D gridTraveler. you begin in the top-left corner 
+// and your goal is to travel to the bottom-right corner.D
+// You may only move down or right
+// In how many ways can you travel to the goal on a grid with dimensions m * n?
+
+//brute force recursion
+// const gridTraveler = (m, n ) => {
+
+//     if(m === 1 && n === 1) return 1;
+//     if (m === 0 || n === 0) return 0;
+//     return gridTraveler(m -1, n) + gridTraveler(m, n-1);
+
+// };
+
+const gridTraveler = (m, n , memo ={}) => {
+    const key = m + ',' + n;
+
+    if(key in memo) return memo[key];
+    if(m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+
+    memo[key] = gridTraveler(m -1, n, memo) + gridTraveler(m, n-1, memo);
+    return memo[key]
+};
+
+console.log(gridTraveler(1,1));//1
+console.log(gridTraveler(2,3));//3
+console.log(gridTraveler(3,2));//3
+console.log(gridTraveler(3,3));//6
+console.log(gridTraveler(18,18));//2333606220
+
+//Time/Space
+// brute force recursion: O(2^n+m)time  O(n+m) space
+
+// memoized O(m*n)time O(n+m) space
