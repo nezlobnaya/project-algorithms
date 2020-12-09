43cab4cef4190084a34127503a4057c2737c4eed
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-08 20:23:28 -0500

    added solution using tabulation to canSum in dynamic prog

--- dynamic_programming/sum/canSum.js
+++ dynamic_programming/sum/canSum.js
@@ -17,30 +17,44 @@ not it is possible to generate the targetSum using numbers from the array*/
 //     return false;
 // }
 
-const canSum = (targetSum, numbers, memo={}) => {
-    if (targetSum in memo) return memo[targetSum];
-    if (targetSum === 0) return true;
-    if (targetSum < 0) return false;
+// const canSum = (targetSum, numbers, memo={}) => {
+//     if (targetSum in memo) return memo[targetSum];
+//     if (targetSum === 0) return true;
+//     if (targetSum < 0) return false;
     
-    for (let num of numbers) {
-        const remainder = targetSum - num;
-        if (canSum(remainder, numbers, memo) === true) {
-            memo[targetSum] = true;
-            return true;
+//     for (let num of numbers) {
+//         const remainder = targetSum - num;
+//         if (canSum(remainder, numbers, memo) === true) {
+//             memo[targetSum] = true;
+//             return true;
+//         }
+
+//     }
+//     memo[targetSum] = false;
+//     return false;
+// }
+
+// with tabulation:
+const canSum = (targetSum, numbers) => {
+    const table  = Array(targetSum + 1).fill(false);
+    table[0] = true;
+    
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] ===true) {
+            for (let num of numbers) {
+                table[i + num] = true
+            }
         }
 
     }
-    memo[targetSum] = false;
-    return false;
+    return table[targetSum];
 }
-
-
 
 console.log(canSum(7, [2, 3]));// true
 console.log(canSum(7, [5, 3, 4, 7]));// true
 console.log(canSum(7, [2, 4]));// false
 console.log(canSum(8, [2, 3, 5]));// true
-console.log(canSum(300, [7, 14]));// true
+console.log(canSum(300, [7, 14]));// false
 
 //Time/Space
 // brute force recursion: O(n^m)time  O(m) space
