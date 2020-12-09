88e2c6d4e4a307876d8ed4005b53133199614a70
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-08 20:01:15 -0500

    structure refactoring

--- compute_the_nth_fibonacci/compute_the_nth_fibonacci.py
+++ compute_the_nth_fibonacci/compute_the_nth_fibonacci.py
@@ -44,29 +44,40 @@ using memoization
 '''
 bottom up approach
 '''
-def fib(n):
-    # Edge cases:
-    if n < 0:
-        raise ValueError('Index was negative. No such thing as a '
-                         'negative index in a series.')
-    elif n in [0, 1]:
-        return n
+# def fib(n):
+#     # Edge cases:
+#     if n < 0:
+#         raise ValueError('Index was negative. No such thing as a '
+#                          'negative index in a series.')
+#     elif n in [0, 1]:
+#         return n
 
-    # We'll be building the fibonacci series from the bottom up
-    # so we'll need to track the previous 2 numbers at each step
-    prev_prev = 0  # 0th fibonacci
-    prev = 1       # 1st fibonacci
+#     # We'll be building the fibonacci series from the bottom up
+#     # so we'll need to track the previous 2 numbers at each step
+#     prev_prev = 0  # 0th fibonacci
+#     prev = 1       # 1st fibonacci
 
-    for _ in range(n - 1):
-        # Iteration 1: current = 2nd fibonacci
-        # Iteration 2: current = 3rd fibonacci
-        # Iteration 3: current = 4th fibonacci
-        # To get nth fibonacci ... do n-1 iterations.
-        current = prev + prev_prev
-        prev_prev = prev
-        prev = current
+#     for _ in range(n - 1):
+#         print("heck", _)
+#         # Iteration 1: current = 2nd fibonacci
+#         # Iteration 2: current = 3rd fibonacci
+#         # Iteration 3: current = 4th fibonacci
+#         # To get nth fibonacci ... do n-1 iterations.
+#         current = prev + prev_prev
+#         prev_prev = prev
+#         prev = current
+#         print("current", current)
+#     return current
 
-    return current
+# using tabulation:
+
+def fib(n):         
+  results = [1, 1]         
+  for i in range(2, n):             
+    results.append(results[i-1] + results[i-2])         
+  return results[-1] 
+
+print(fib(8))
 
 # import math 
 # def fib(n):
@@ -77,6 +88,7 @@ def fib(n):
 
 for i in range(8):
     print(f'Number{i}: {fib(i)}')
+print(fib(8))
 
 print('Print 1st', fib(1))
 print('Print 2nd', fib(2))
@@ -84,6 +96,8 @@ print('Print 3rd', fib(3))
 print('Print 4th', fib(4))
 print('Print 5th', fib(5))
 print('Print 6th', fib(6))
+print('Print 7th', fib(7))
+print('Print 8th', fib(8))
 
 # Time complexity O(n) 
 # Space complexity O(1) good!
