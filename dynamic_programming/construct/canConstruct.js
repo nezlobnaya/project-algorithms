9bf327969bb8f31c3fd548b5d4a4716e4b8d1fc9
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-15 21:03:11 -0500

    solution using tabulation added to canConstruct

--- dynamic_programming/construct/canConstruct.js
+++ dynamic_programming/construct/canConstruct.js
@@ -24,22 +24,42 @@ You may reuse elememts of `wordBank` as many times as needed.
 //     return false;
 // }
 
-const canConstruct = (target, wordBank, memo={}) => {
-    if (target in memo) return memo[target];
-    if (target === "") return true;
+// const canConstruct = (target, wordBank, memo={}) => {
+//     if (target in memo) return memo[target];
+//     if (target === "") return true;
 
-    for (let word of wordBank) {
-        if (target.indexOf(word) === 0) {
-            const suffix = target.slice(word.length);
-            if (canConstruct(suffix , wordBank, memo) === true) {
-                memo[target] = true;
-                return true;
+//     for (let word of wordBank) {
+//         if (target.indexOf(word) === 0) {
+//             const suffix = target.slice(word.length);
+//             if (canConstruct(suffix , wordBank, memo) === true) {
+//                 memo[target] = true;
+//                 return true;
+//             }
+//         }
+//     }
+//     memo[target] = false;
+//     return false;
+// }
+
+const canConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1).fill(false);
+    table[0] = true;
+
+
+    for(let i = 0; i<= target.length; i++) {
+            if (table[i] === true) {
+                for (let word of wordBank) {
+                    //if the word matches characters starting at the position i
+                    if ( target.slice(i, i + word.length) === word) {
+                        table[i + word.length] = true;
+                    }
+                }
             }
+            
         }
+        return table[target.length];
     }
-    memo[target] = false;
-    return false;
-}
+
 
 
 console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));// true
@@ -61,3 +81,9 @@ console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
 //Space complexity 
 //brute force O(m^2)
 //memoize O(m^2)
+
+//Tabulation
+//Time complexity
+// O(m^2*n)
+//Space complexity 
+//O(m)
