067e048f85acb561b0e63dffb681639c6aca75b3
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-02-19 12:55:22 -0500

    another solution to longest word in a string

--- longest_word.py
+++ longest_word.py
@@ -3,31 +3,35 @@ find the longest word in a  string!
 
 """
 
-import re
+# import re
+# def LongestWord(sen):
+#   regex = re.compile('[^a-zA-Z]')
+#   s = sen.split()
+#   hash = {}
+#   for word in s:
+#     if word.isalnum():
+#       hash[word] = len(word)
+#     else: 
+#       word = regex.sub('', word)
+#       hash[word] = len(word)
+#   max_v = -10000000000000
+#   for v in hash.values():
+#     max_v = max(max_v, v)
+#   for k, v in hash.items():
+#     if v == max_v:
+#       return k
+
 def LongestWord(sen):
-  regex = re.compile('[^a-zA-Z]')
-  s = sen.split()
-  hash = {}
-  for word in s:
-    if word.isalnum():
-      hash[word] = len(word)
+  ans =''
+  for char in sen:
+    if char.isalnum():
+      ans+=char
     else: 
-      word = regex.sub('', word)
-      hash[word] = len(word)
-  max_v = -10000000000000
-  for v in hash.values():
-    max_v = max(max_v, v)
-  for k, v in hash.items():
-    if v == max_v:
-      return k
-
-    
-
-  # code goes here
-  # return sen
+      ans+=' '
+  return max(ans.split(), key =len)
 
 # keep this function call here 
 def main():
-    sen = "I am a very! long? sentence...!!!!"
+    sen = "I am a very(!) long? 'sentence'...!!!!"
     print(LongestWord(sen))
 main()
