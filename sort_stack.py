61f27d9ae081d26e122029b01d98d1d759ab5e40
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-05-18 22:05:51 -0400

    another solution for sorting a stack added

--- sort_stack.py
+++ sort_stack.py
@@ -30,6 +30,28 @@ def sortStack(stack: Stack) -> Stack:
     stack.push(aux.pop())
   return stack
 
+# def sort_stack(input_stack):
+#   # init second stack
+#   output_stack = Stack()
+#   # loop until the first stack is empty
+#   # also make sure that we don't leave anything in temp
+#   temp = None
+#   while not input_stack.is_empty():
+#     #  put the top of the first stack in a temp var
+#     if temp is None:
+#       temp = input_stack.pop()
+#     # if the second stack is empty OR temp > the top of the second stack
+#     if output_stack.is_empty() or temp > output_stack.peek():
+#       # push temp to the second stack
+#       output_stack.push(temp)
+#       temp = None
+#     # else:
+#     else:
+#       # pop the top of the second and push to the first
+#       input_stack.push(output_stack.pop())
+      
+#   return output_stack
+  
 s = Stack()
 s.push(4)
 s.push(10)
